f5939127becc71692bf4f45938c7e1af8ca46ddf
imniladri <jnvniladri101@gmail.com>
2021-04-23 23:07:29 +0530

    Added Hover Effect in Skills

---
+++ js/iconsHover.js
@@ -0,0 +1,106 @@
+// Mouse Leave (Hover Out)
+
+$(".skill:nth-child(n)").mouseleave(function () {
+    $(".skill:nth-child(n)").css({
+        transform: "scale(1)",
+        filter: "grayscale(0)",
+    });
+});
+
+// Mouse Enter (Hover In)
+
+// Skill 1 : HTML5
+$(".skill:nth-child(1)").mouseenter(function () {
+    $(".skill:nth-child(1)").css({
+        transform: "scale(1.1)",
+        filter: "grayscale(0)",
+    });
+    $(".skill:not(:nth-child(1))").css({
+        transform: "scale(0.8)",
+        filter: "grayscale(1)",
+    });
+});
+
+// Skill 2 : CSS3
+$(".skill:nth-child(2)").mouseenter(function () {
+    $(".skill:nth-child(2)").css({
+        transform: "scale(1.1)",
+        filter: "grayscale(0)",
+    });
+    $(".skill:not(:nth-child(2))").css({
+        transform: "scale(0.8)",
+        filter: "grayscale(1)",
+    });
+});
+
+// Skill 3 : Javascript
+$(".skill:nth-child(3)").mouseenter(function () {
+    $(".skill:nth-child(3)").css({
+        transform: "scale(1.1)",
+        filter: "grayscale(0)",
+    });
+    $(".skill:not(:nth-child(3))").css({
+        transform: "scale(0.8)",
+        filter: "grayscale(1)",
+    });
+});
+
+// Skill 4 : Node JS
+$(".skill:nth-child(4)").mouseenter(function () {
+    $(".skill:nth-child(4)").css({
+        transform: "scale(1.1)",
+        filter: "grayscale(0)",
+    });
+    $(".skill:not(:nth-child(4))").css({
+        transform: "scale(0.8)",
+        filter: "grayscale(1)",
+    });
+});
+
+// Skill 5 : Mongo DB
+$(".skill:nth-child(5)").mouseenter(function () {
+    $(".skill:nth-child(5)").css({
+        transform: "scale(1.1)",
+        filter: "grayscale(0)",
+    });
+    $(".skill:not(:nth-child(5))").css({
+        transform: "scale(0.8)",
+        filter: "grayscale(1)",
+    });
+});
+
+// Skill 6 : Express JS
+$(".skill:nth-child(6)").mouseenter(function () {
+    $(".skill:nth-child(6)").css({
+        transform: "scale(1.1)",
+        filter: "grayscale(0)",
+    });
+    $(".skill:not(:nth-child(6))").css({
+        transform: "scale(0.8)",
+        filter: "grayscale(1)",
+    });
+});
+
+// Skill 7 : React JS
+$(".skill:nth-child(7)").mouseenter(function () {
+    $(".skill:nth-child(7)").css({
+        transform: "scale(1.1)",
+        filter: "grayscale(0)",
+    });
+    $(".skill:not(:nth-child(7))").css({
+        transform: "scale(0.8)",
+        filter: "grayscale(1)",
+    });
+});
+
+// Skill 8 : Python
+$(".skill:nth-child(8)").mouseenter(function () {
+    $(".skill:nth-child(8)").css({
+        transform: "scale(1.1)",
+        filter: "grayscale(0)",
+    });
+    $(".skill:not(:nth-child(8))").css({
+        transform: "scale(0.8)",
+        filter: "grayscale(1)",
+    });
+});
