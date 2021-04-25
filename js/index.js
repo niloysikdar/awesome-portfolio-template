6417933bee4290b538c831c5d26ac0f986c07966
imniladri <jnvniladri101@gmail.com>
2021-04-25 13:22:04 +0530

    Added Loader

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+$(window).on("load", function () {
+    $("#preloader").css({
+        transform: "translateY(-100%)",
+        "transition-delay": "0.6s",
+    });
+    $(".loader").css({
+        opacity: "0",
+        "transition-delay": "0.3s",
+    });
+});
+
 $(window).scroll(function () {
     $("header").toggleClass("padding", window.scrollY > 100);
 });
